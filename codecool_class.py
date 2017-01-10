15285a8fdb8f6843279e7717990652e0bce5ddbb
pawellasota <plasota76@gmail.com>
2017-01-10 19:28:49 +0100

    Codecool class base implementation

--- codecool_class.py
+++ codecool_class.py
@@ -3,4 +3,24 @@ from student import Student
 
 
 class CodecoolClass:
-    pass
+
+    def __init__(self, location, year, students):
+        self.location = location
+        self.year = year
+        self.students = students
+
+    @staticmethod
+    def create_local_school():
+        students = Student.create_by_csv("data/students.csv")
+        #mentors = Mentor.create_by_csv()
+        return students
+
+    def find_student_by_full_name(self, full_name):
+        name_list = full_name.split(" ")
+        for student in self.students:
+            if student.first_name == name_list[0]:
+                if student.last_name == name_list[1]:
+                    print(student.first_name, student.last_name + " found in our class.")
+                    return student
+        print(full_name + " student is not in our class.")
+        return False
--- story.py
+++ story.py
@@ -2,4 +2,7 @@ from codecool_class import CodecoolClass
 from mentor import Mentor
 from student import Student
 
-codecool_krk = CodecoolClass.create_local_school
+
+codecool_krk = CodecoolClass.create_local_school()
+codecoolObject = CodecoolClass("Krakow", 2016, codecool_krk)
+codecoolObject.find_student_by_full_name("Marek Frankowicz")
