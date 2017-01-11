738ccbc1b279c56e73804a6630bd47b2cc5be822
pawellasota <plasota76@gmail.com>
2017-01-11 14:07:11 +0100

    fixed give_motivational_speech function

--- story.py
+++ story.py
@@ -1,11 +1,16 @@
 from codecool_class import CodecoolClass
 import random
+import time
 from mentor import Mentor
 from student import Student
 
 
-codecool_krk = CodecoolClass.create_local_school()
+print("Uploading list of mentors („Mentors are initialized from CSV”)")
+print("Uploading list of students („Students are initialized from CSV”)")
+time.sleep(2)
 codecoolObject = CodecoolClass("Krakow", 2016, codecool_krk[0], codecool_krk[1])
+codecool_krk = CodecoolClass.create_local_school()
+print(codecool_krk)
 codecoolObject.find_student_by_full_name("Marek Frankowicz")
 codecoolObject.find_mentor_by_full_name("Przemek Ciacka")
 codecoolObject.check_overall_energy()
