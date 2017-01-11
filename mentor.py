83620342b15577e796910cae4a15d42ac7ec49f6
Anna Matras <ania.matras@gmail.com>
2017-01-11 10:43:47 +0100

    mentor module with csv metod

--- mentor.py
+++ mentor.py
@@ -1,5 +1,24 @@
 from person import Person
+import csv
 
 
 class Mentor(Person):
-    pass
+    def __init__(self, first_name, last_name, year_of_birth, gender, nickname, energy_level, humanity_level):
+        Person.__init__(self, first_name, last_name, year_of_birth, gender)
+        self.nickname = nickname
+        self.energy_level = energy_level
+        self.humanity_level = humanity_level
+
+    @staticmethod
+    def create_by_csv(filename="data/mentors.csv"):
+        """Loading data from csv file"""
+        list_of_mentors_object = []
+        with open(filename) as file:
+            file_reader =  csv.reader(file)
+            for data in file_reader:
+                list_of_mentors_object.append(Mentor(data[0], data[1], data[2], data[3], data[4], data[5], data[6])
+        return list_of_mentors_object
+
+
+# Mentor.create_by_csv(filename="data/mentors.csv")
+
