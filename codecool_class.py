6fa442afc056a06a0f1b3ccca1db49e065a1d410
Anna Matras <ania.matras@gmail.com>
2017-01-13 09:28:10 +0100

    String format

--- codecool_class.py
+++ codecool_class.py
@@ -50,7 +50,7 @@ class CodecoolClass:
     def is_any_mentor_became_human(self):
         for mentor in self.mentors:
             if mentor.humanity_level >= 10:
-                print(mentor.first_name, mentor.last_name+" called " + mentor.nickname+" has become human! "
+                print("\033[44m"+mentor.first_name, mentor.last_name+" called " + mentor.nickname+" has become human! "
                         "Is ready to deliver to new Codecool facility!", mentor.first_name, mentor.last_name,
                         "may the Force be with You!")
                 return True
